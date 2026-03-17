427e2059a698505955c6bfd47047b352df3dec3c
Orfeouso <sahibjitg3@gmail.com>
2026-03-17 13:09:08 -0400

    Created finger-exercises folder and finger-exercise-1

---
+++ finger-exercises/finger-exercise-1/fe1.py
@@ -0,0 +1,17 @@
+#Finger Exercise 1
+#Sahibjit Guraya
+#5 minutes
+
+#Define variable a, b, c
+#Add a and b, multiply sum by c
+#prints variable 'total'
+
+
+a = 5
+b = 10
+c = 6
+
+total = (a + b) * c
+
+print(total)
+
